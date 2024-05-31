de69ff1c34370490b9e2559a8847615acc827d93
unknown <jployola@pucp.edu.pe>
2024-05-30 20:51:15 -0500

    borrado

--- src/main/java/org/example/gtics_lab7/Controller/UsuarioController.java
+++ src/main/java/org/example/gtics_lab7/Controller/UsuarioController.java
@@ -1,15 +1,16 @@
 package org.example.gtics_lab7.Controller;
 
 
+import org.apache.coyote.Response;
 import org.example.gtics_lab7.Entity.Repositorio;
 import org.example.gtics_lab7.Entity.Usuario;
 import org.example.gtics_lab7.Repository.UsuarioRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 
 @RestController
@@ -61,5 +62,25 @@ final UsuarioRepository usuarioRepository;
         }
         else
             return "redirect:/Usuario";
+    }
+
+    @DeleteMapping(value = "/Usuario/Eliminar/{id}")
+    public ResponseEntity<HashMap<String, Objects>> borrarUsuario(@PathVariable("id") String idstr){
+        HashMap<String, Objects> responseMap = new HashMap<>();
+
+        try {
+            int id=Integer.parseInt(idstr);
+            if(usuarioRepository.existsById(id)){
+                usuarioRepository.deleteById(id);
+                responseMap.put("estado", "Eliminado");
+                return ResponseEntity.ok(responseMap);
+            }else {
+                responseMap.put("estado", "error");
+                return ResponseEntity.badRequest().body(responseMap);
+            }
+        } catch (NumberFormatException ex){
+            responseMap.put("estado","error");
+            return ResponseEntity.badRequest().body(responseMap);
+        }
     }
 }
