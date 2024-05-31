e83e504aded6b2d242e0ca43817dac6de2e298be
unknown <jployola@pucp.edu.pe>
2024-05-30 19:27:26 -0500

    1

---
+++ src/main/java/org/example/gtics_lab7/GticsLab7Application.java
@@ -0,0 +1,13 @@
+package org.example.gtics_lab7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GticsLab7Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/gtics_lab7/GticsLab7ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gtics_lab7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
